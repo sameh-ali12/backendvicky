d499cc44d0eee30361acb062444f634396a70795
sameh-ali12 <119759726+sameh-ali12@users.noreply.github.com>
2025-10-29 18:43:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.use(function (req, res, next) {
   
   // Website you wish to allow to connect
   // res.setHeader('Access-Control-Allow-Origin', 'https://sales-art-bc8a3.web.app');
-   res.setHeader('Access-Control-Allow-Origin', 'https://vicky-dariy.web.app');
+   res.setHeader('Access-Control-Allow-Origin', 'https://vickyvicky.web.app/');
    
    // res.setHeader('Access-Control-Allow-Origin', 'https://sales-art-bc8a3.web.app');
 
